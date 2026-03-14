92470cf0af90a5f6ba33d396d0c0b00e791640a8
Keshav-Satya <nit.memories825@gmail.com>
2026-03-14 13:48:37 +0530

    app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ text_llm = LLM(
 # vision_model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Naya Updated Code:
-vision_model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+vision_model = genai.GenerativeModel('gemini-1.5-flash-8b')
+
 
 # 5. Search Tool
 @tool('search_tool')
@@ -133,6 +134,7 @@ with tab2:
                 
             st.markdown(f'<div class="report-card"><h3>🔍 Image Analysis Report</h3>{response.text}</div>', unsafe_allow_html=True)
             st.balloons()
+
 
 
 
